d05c7c6ac397100bd59b2269bfeedc6a44c063a9
timickb <timikm2002@gmail.com>
2019-04-26 22:51:49 +0500

    init

---
+++ app/src/main/java/com/example/baseconverter/Converter.java
@@ -0,0 +1,35 @@
+package com.example.baseconverter;
+
+import java.util.HashMap;
+
+public class Converter {
+    private String number;
+    private int fromBase, toBase;
+
+    public Converter(String number, int fromBase, int toBase) {
+        this.number = number;
+        this.fromBase = fromBase;
+        this.toBase = toBase;
+    }
+    public boolean valid() {
+        if(toBase > 36 || fromBase > 36) return false;
+        for(int i = 0; i < number.length(); i++) {
+            if(Values.table.get(number.charAt(i)) == null)
+                return false;
+        }
+        return true;
+    }
+
+    public String convert() {
+        int base10 = 0;
+        for(int i = 0; i < number.length(); i++) {
+            base10 += Values.table.get(number.charAt(i))*Math.pow(fromBase, number.length()-i-1);
+        }
+        String result = "";
+        while(base10 > 0) {
+            result =  Values.reversedTable.get(base10 % toBase) + result;
+            base10 /= toBase;
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/baseconverter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.baseconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button convertBtn;
+    private ImageView swapBtn;
+    private EditText numberInput, fromInput, toInput;
+    private TextView resultLabel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        convertBtn = findViewById(R.id.btn_convert);
+        swapBtn = findViewById(R.id.btn_swap);
+        numberInput = findViewById(R.id.input_number);
+        fromInput = findViewById(R.id.input_from);
+        toInput = findViewById(R.id.input_to);
+        resultLabel = findViewById(R.id.label_result);
+
+        swapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmp = fromInput.getText().toString();
+                fromInput.setText(toInput.getText().toString());
+                toInput.setText(tmp);
+
+            }
+        });
+
+        convertBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Converter c = new Converter(numberInput.getText().toString(),
+                Integer.parseInt(fromInput.getText().toString()),
+                Integer.parseInt(toInput.getText().toString()));
+
+        if(!c.valid()) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.incorrect_value, Toast.LENGTH_SHORT).show();
+        } else {
+            resultLabel.setText(c.convert());
+        }
+    }
+}
